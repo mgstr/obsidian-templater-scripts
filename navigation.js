ef8d2da88ee10bce49ece25260c049180914e5c9
mgstr <1258626+mgstr@users.noreply.github.com>
2023-11-10 23:28:41 +0200

    refactirubg

--- navigation.js
+++ navigation.js
@@ -1,20 +1,7 @@
-// <% tp.user.navigation(tp.file.title) %>
-// <% tp.user.navigation(tp.file.title, "ru") %>
-// <% tp.user.navigation(tp.file.title, "est", {weekday:"long"}) %>
-
-const day_in_milis = 24 * 60 * 60 * 1000;
-function e2d(d) {
-    return new Date(d);
-}
-function e2title(e) {
-    return e2d(e).toISOString().slice(0, 10);
-}
-function e2weekday(e) {
-    return e2d(e).getDay();
-}
-function isWorkingDay(wd) {
-    return wd >= 1 && wd <= 5;
-}
+const e2d = (e) => new Date(e);
+const e2title = (e) => e2d(e).toISOString().slice(0, 10);
+const e2weekday = (e) => e2d(e).getDay();
+const isWorkingDay = (wd) => wd >= 1 && wd <= 5;
 function get_next_day(e, delta) {
     const next = e + delta;
     const wd = e2weekday(next);
@@ -24,17 +11,14 @@ function get_next_day(e, delta) {
     return get_next_day(next, delta);
 }
 function navigation(title, locale, weekday={weekday:"short"}) {
-    function e2weekday_name(e) {
-        return e2d(e).toLocaleDateString(locale, weekday);
-    }
-    function link(e) {
-        return `[[${e2title(e)}|${e2weekday_name(e)}]]`;
-    }
+    const e2weekday_name = (e) => e2d(e).toLocaleDateString(locale, weekday);
+    const link = (e) => `[[${e2title(e)}|${e2weekday_name(e)}]]`;
     
     const e = Date.parse(title);
     if (isNaN(e)) {
         return `error: invalid title format, expecting YYYY-MM-DD, but was: ${title}`;
     }
+    const day_in_milis = 24 * 60 * 60 * 1000;
     y = get_next_day(e, -day_in_milis);
     t = get_next_day(e, +day_in_milis);
     return `${link(y)} ${e2weekday_name(e)} ${link(t)}`;
