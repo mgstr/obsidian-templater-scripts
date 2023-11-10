e43e8d5ea1659bd43caa8dd4cc538b633bedd8f3
mgstr <1258626+mgstr@users.noreply.github.com>
2023-11-10 23:35:48 +0200

    refactoring

--- navigation.js
+++ navigation.js
@@ -2,26 +2,25 @@ const e2d = (e) => new Date(e);
 const e2title = (e) => e2d(e).toISOString().slice(0, 10);
 const e2weekday = (e) => e2d(e).getDay();
 const isWorkingDay = (wd) => wd >= 1 && wd <= 5;
-function get_next_day(e, delta) {
+function getNextWorking(e, delta) {
     const next = e + delta;
-    const wd = e2weekday(next);
-    if (isWorkingDay(wd)) {
+    if (isWorkingDay(e2weekday(next))) {
         return next;
     }
-    return get_next_day(next, delta);
+    return getNextWorking(next, delta);
 }
 function navigation(title, locale, weekday={weekday:"short"}) {
-    const e2weekday_name = (e) => e2d(e).toLocaleDateString(locale, weekday);
-    const link = (e) => `[[${e2title(e)}|${e2weekday_name(e)}]]`;
+    const e2weekdayName = (e) => e2d(e).toLocaleDateString(locale, weekday);
+    const link = (e) => `[[${e2title(e)}|${e2weekdayName(e)}]]`;
     
     const e = Date.parse(title);
     if (isNaN(e)) {
         return `error: invalid title format, expecting YYYY-MM-DD, but was: ${title}`;
     }
     const day_in_milis = 24 * 60 * 60 * 1000;
-    y = get_next_day(e, -day_in_milis);
-    t = get_next_day(e, +day_in_milis);
-    return `${link(y)} ${e2weekday_name(e)} ${link(t)}`;
+    const y = getNextWorking(e, -day_in_milis);
+    const t = getNextWorking(e, +day_in_milis);
+    return `${link(y)} ${e2weekdayName(e)} ${link(t)}`;
 }
 
 module.exports = navigation;
